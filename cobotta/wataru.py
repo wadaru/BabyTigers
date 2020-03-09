c2f2c625aaaffdd66ba4eebda7237fa76842c052
Wataru UEMURA <wataru@rins.ryukoku.ac.jp>
2020-03-09 16:34:18 +0900

    remove non-used codes.

--- cobotta/wataru.py
+++ cobotta/wataru.py
@@ -30,13 +30,6 @@ joints_name = ["joint_1", "joint_2",
                "joint_3", "joint_4", "joint_5", "joint_6"]
 
 #
-# Poses
-#
-joints_packing_old = [90, -60, 125, 90, -95, 0]
-joints_packing_new = [90, -30, 120, -170, -94, 0]
-joints_home = [0, 30, 100, 0, 50, 0]
-
-#
 # Parallel gripper
 #
 gripper_parallel_open = 0.015
@@ -58,62 +51,8 @@ def gripper_move(gripper_client, width, speed, effort):
     goal.effort = effort
     gripper_client.send_goal(goal)
 
-
-def is_motor_running():
-    rospy.wait_for_service('/cobotta/get_motor_state', 3.0)
-    try:
-        get_motor_state = rospy.ServiceProxy('/cobotta/get_motor_state',
-                                             GetMotorState)
-        res = get_motor_state()
-        return res.state
-    except rospy.ServiceException, e:
-    # except rospy.SeriveException as e:
-        print >> sys.stderr, "  Service call failed: %s" % e
-        # print(" Service call failed: ", e, file=sys.stderr)
-
-def is_simulation():
-    service_list = rosservice.get_service_list()
-    if '/cobotta/get_motor_state' in service_list:
-        return False
-    return True
-
-def is_button_on(plus_button, minus_button, function_button, joint_state):
-    # nothing
-    if plus_button == 2:
-        print "debug"
-
-def is_button_on_mute(plus_button, minus_button, function_button, joint_state):
-    if plus_button == True:
-        print "plus: true,  ", 
-        # print("plue: true,  ", end = "")
-    else:
-        print "plus: false, ", 
-        # print("plue: false, ", end = "")
-    if minus_button == True:
-        print "minus: true,  ", 
-        # print("minus: true,  ", end = "")
-    else:
-        print "minus: false, ", 
-        # print("minus: false, ", end = "")
-    if function_button == True:
-        print("function: true,  ")
-    else:
-        print("function: false, ")
-    print joint_state
-    # print(joint_state)
-
 if __name__ == '__main__':
     rospy.init_node("packing_pose")
-    # rospy.Subscriber('/cobotta/plus_button', Bool, is_plus_button_on)
-    sub1 = message_filters.Subscriber('/cobotta/plus_button', Bool)
-    sub2 = message_filters.Subscriber('/cobotta/minus_button', Bool)
-    sub3 = message_filters.Subscriber('/cobotta/function_button', Bool)
-    sub4 = message_filters.Subscriber('/cobotta/joint_states', JointState)
-    fps = 1
-    delay = 1 / fps * 0.5
-    ts = message_filters.ApproximateTimeSynchronizer([sub1, sub2, sub3, sub4], 10, delay, allow_headerless = True)
-    ts.registerCallback(is_button_on)
-
 
     moveit_commander.roscpp_initialize(sys.argv)
     move_group = moveit_commander.MoveGroupCommander("arm")
@@ -131,7 +70,7 @@ if __name__ == '__main__':
       recvPORT = 9182
       sendADDRESS = "127.0.1.1"
       # recvADDRESS = "127.0.1.1"
-      recvADDRESS = "192.168.11.60"
+      recvADDRESS = "192.168.11.60" # this machine's ip address.
 
     print("sendADD:", sendADDRESS, ", sendPORT:", sendPORT)
     print("recvADD:", recvADDRESS, ", recvPORT:", recvPORT)
