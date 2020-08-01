4ee7bc515ae168765bea75ae8f70aeaf7dd930d1
Wataru UEMURA <wataru@rins.ryukoku.ac.jp>
2020-08-01 10:04:14 +0900

    find the closest object in the circle view.

--- cobotta/d435.py
+++ cobotta/d435.py
@@ -33,6 +33,10 @@ distance_min = 10.0 / depth_scale
 distance_max = TARGET_DISTANCE / depth_scale
 print("Depth Scale = {} -> {}".format(depth_scale, distance_max))
 
+bg_image = np.full((HEIGHT, WIDTH), 0, dtype = np.uint8)
+null_image = np.full((HEIGHT, WIDTH, 3), 100, dtype = np.uint8)
+cv2.circle(bg_image, (WIDTH /2 , HEIGHT / 2), HEIGHT / 4, 1, thickness = -1, lineType = cv2.LINE_8)
+cv2.circle(null_image, (WIDTH /2 , HEIGHT / 2), HEIGHT / 4, (127, 127, 127), thickness = -1, lineType = cv2.LINE_8)
 
 try:
     while True:
@@ -46,28 +50,31 @@ try:
         color_image = np.asanyarray(color_frame.get_data())
         # processing before Depth(within 2m)
         depth_image = np.asanyarray(depth_frame.get_data())
+        depth_image = depth_image * bg_image
         min_depth_image = (depth_image < 160.0) * distance_max + depth_image
         minDistance = np.amin(min_depth_image)
-        # print(minDistance)
-
-        depth_image = (depth_image < distance_max) * depth_image
-        depth_graymap = (min_depth_image <= (minDistance + 0.05)) * 127. + depth_image * 127. / distance_max
-        depth_graymap = depth_graymap.reshape((HEIGHT, WIDTH)).astype(np.uint8)
-        depth_colormap = cv2.cvtColor(depth_graymap, cv2.COLOR_GRAY2BGR)
-
         findX = -1
         findY = -1
-        for x in range(0, WIDTH):
-            for y in range(0, HEIGHT):
-                if (min_depth_image[y, x] == minDistance):
-                    findX = x
-                    findY = y
-                    depth_colormap[y ,x] = [127, 127, 255]
-        print("x = ", findX, ", y = ", findY)
+        if (minDistance < distance_max - 1):
+            depth_image = (depth_image < distance_max) * depth_image
+            depth_graymap = ((min_depth_image <= (minDistance + 0.05)) * 127. + depth_image * 127. / distance_max) 
+            depth_graymap = depth_graymap.reshape((HEIGHT, WIDTH)).astype(np.uint8)
+            depth_colormap = cv2.cvtColor(depth_graymap, cv2.COLOR_GRAY2BGR)
+
+            for x in range(0, WIDTH):
+                for y in range(0, HEIGHT):
+                    if (min_depth_image[y, x] == minDistance):
+                        findX = x
+                        findY = y
+                        depth_colormap[y ,x] = [127, 127, 255]
+        else:
+            depth_colormap = null_image
+            minDistance = -1
 
         images = np.hstack((color_image, depth_colormap))
         cv2.namedWindow("RealSense", cv2.WINDOW_AUTOSIZE)
         cv2.imshow("RealSense", images)
+        print("x = ", findX, ", y = ", findY, "dist = ", minDistance)
 
         if cv2.waitKey(1) & 0xff == 27:
             break
